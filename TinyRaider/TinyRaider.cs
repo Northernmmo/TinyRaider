21d03810d6270906a55287622ce11e0599e037d0
Hewk <northernmmo@gmail.com>
2016-10-16 12:46:24 -0800

    Basic CR file structure.

---
+++ TinyRaider/TinyRaider.cs
@@ -0,0 +1,25 @@
+﻿using Styx;
+using Styx.CommonBot.Routines;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TinyRaider
+{
+	public partial class TinyRaider : CombatRoutine
+	{
+		public override string Name { get { return "[Hewk] TinyRaider v1.0"; } }
+
+		public override WoWClass Class { get { return StyxWoW.Me.Class ; } }
+
+		public override CapabilityFlags SupportedCapabilities
+		{
+			get
+			{
+				return CapabilityFlags.All;
+			}
+		}
+	}
+}
